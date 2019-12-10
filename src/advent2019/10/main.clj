4b44cff858fe26e25362c6621d9d1b832b972178
Jake Davies <jgdavies24@gmail.com>
2019-12-10 10:53:42 -0800

    part 1

--- src/advent2019/10/main.clj
+++ src/advent2019/10/main.clj
@@ -12,16 +12,40 @@
 
 (def initial-input (map (fn [input] (str/split input #"")) (str/split-lines (slurp url))))
 
-(defn los [a1 a2]
-  ())
+(defn abs [n] (max n (- n)))
+
+(defn angle [[o1 o2] [d1 d2]]
+  (Math/atan2 (- o1  d1) (- o2 d2)))
+
 ; for every asteroid, we check every other asteroid and then check asteroids where the x and y are between the two current asteroids 
+
+(defn inclusive-range
+  ([] (range))
+  ([end] (range (inc end)))
+  ([start end] (range start (inc end)))
+  ([start end step] (range start (+ end step) step)))
 
 (def asteroid-map (into {}  (flatten (for [y (range (count initial-input))]
                                        (for [x (range (count (nth initial-input 0)))]
                                          {[x y] (= "#" (nth (nth initial-input y) x))})))))
 
-(asteroid-map [1 0])
 (def asteroids  (into {} (filter #(boolean (second %))) asteroid-map))
-asteroids
 
-(asteroids [1 0])
+(defn los [[x1 y1] [x2 y2]]
+  (= 1 (count  (->> (let [t (angle [x1 y1] [x2 y2])]
+                      (for [x (inclusive-range x1 x2 (if (> x1 x2) -1 1))]
+                        (for [y (inclusive-range y1 y2 (if (> y1 y2) -1 1))]
+                          (if (and (asteroids [x y]) (= t (angle [x1 y1] [x y])))
+                            [x y]
+                            false))))
+
+                    (mapcat identity)
+                    (filter #(boolean %))))))
+
+(defn count-los [asteroid]
+  (reduce
+   #(if (los (key asteroid) (key %2)) (+ %1 1) %1)
+   0
+   asteroids))
+
+(println (apply max (map count-los asteroids)))
